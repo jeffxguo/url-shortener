0dd7d8ae4f735ce97cf7ea06c2f7c7c728b6e323
Jeffrey Guo <53244314+jeffxguo@users.noreply.github.com>
2025-09-13 22:57:21 -0700

    Enable SSL for RDS database connection

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,10 @@ const pool = new Pool({
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    port: parseInt(process.env.DB_PORT || '5433'),
+    port: parseInt(process.env.DB_PORT || '5432'),
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 async function initializeDatabase() {
